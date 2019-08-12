2a914f0a8a4cf68236a01fd507d9ae9c54a344ee
Балабанов Виталий Геннадьевич <balabanov_v@tass.ru>
2019-08-12 12:15:22 +0300

    minor changes

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,22 @@ const settings = require('./settings');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // disable SSL alerts
 
-// Получаем аргументы
+// Get command-line arguments
 const server = process.argv.length > 3 && process.argv[3] ? process.argv[3].slice(2) : 'default';
-const summary = process.argv.length > 4 && process.argv[4] ? process.argv[4] : 'Stream-обновление';
+const summary = process.argv.length > 4 && process.argv[4] ? process.argv[4] : 'Page streaming';
 
+// Stream files
 gulp.task('stream', () => watch([
   `${settings[server].articles_path}/**/**/**`,
   `!${settings[server].articles_path}/.git/`,
 ], { ignoreInitial: true, events: ['change'], verbose: true }, (file) => {
-  // Получаем название страницы
+
+  // Prepare
   const pageName = Page.getPagenameByFilepath(file.basename);
   console.log('>>> Open stream for page:', pageName);
-
-  // Создаём страницу
   const p = new Page({ name: pageName, text: file.contents });
 
-  // Подключаемся и меняем страницу
+  // Make changes
   const bot = new MWBot({
     apiUrl: settings[server].server_api,
   });
