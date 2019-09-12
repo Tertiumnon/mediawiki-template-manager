38f7c82a4416d5c45956a83970ca885b28580828
Балабанов Виталий Геннадьевич <balabanov_v@tass.ru>
2019-09-12 17:02:20 +0300

    breaking: redesigned init command

--- components/page-storage/page-storage-init.js
+++ components/page-storage/page-storage-init.js
@@ -5,10 +5,11 @@ const settings = require('../../settings');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // Disable SSL alerts
 
-// Получаем аргументы
+// Arguments
 const server = process.argv.length > 2 && process.argv[2] ? process.argv[2].slice(2) : 'default';
 const dataPath = path.join(__dirname, `../../${settings[server].articles_path}`);
 
+// Create storage instance
 const pageStorage = new PageStorage({
   dataPath,
   apiURL: `${settings[server].server_api}`,
@@ -17,5 +18,11 @@ const pageStorage = new PageStorage({
     password: `${settings[server].bot_password}`,
   },
 });
-pageStorage.init();
-pageStorage.download(pageStorage.getPageList());
+
+// Do some magic
+const ps = pageStorage.init();
+ps.then(() => {
+  pageStorage.download(pageStorage.getPageList());
+}).catch((err) => {
+  console.log(err);
+});
--- components/page-storage/page-storage.js
+++ components/page-storage/page-storage.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 const MWBot = require('mwbot');
 const Page = require('../page/page');
@@ -19,6 +20,7 @@ module.exports = class PageStorage {
     this.dataPath = params.dataPath;
     this.apiURL = params.apiURL;
     this.ignoredPaths = [];
+    this.localData = [];
     this.data = [];
     // bot
     this.botData = params.botData;
@@ -32,8 +34,71 @@ module.exports = class PageStorage {
    */
   init() {
     const { dataPath } = this;
-    this.getLocalPages(dataPath);
-    console.log('PageStorage initialized');
+    return this.getAllTemplates()
+      .then(() => this.getAllCategories())
+      .then(() => this.getAllMediawikis())
+      .then(() => this.getAllProperties())
+      .then(() => {
+        this.initLocalData(dataPath);
+        this.removeNotExistLocalPages();
+        return true;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getAllCategories() {
+    return this.getAllPages({ ns: 14 });
+  }
+
+  getAllTemplates() {
+    return this.getAllPages({ ns: 10 });
+  }
+
+  getAllProperties() {
+    return this.getAllPages({ ns: 102 });
+  }
+
+  getAllMediawikis() {
+    return this.getAllPages({ ns: 8 });
+  }
+
+  getAllPages(params) {
+    return this.bot.loginGetEditToken({
+      apiUrl: this.apiURL,
+      username: this.botData.user,
+      password: this.botData.password,
+    })
+      .then(() => this.bot.request({
+        action: 'query',
+        list: 'allpages',
+        apnamespace: params.ns,
+        aplimit: '5000',
+      }))
+      .then((response) => {
+        if (response.continue) {
+          console.warn('There are too much pages!', params);
+        }
+        if (response.query && response.query.allpages) {
+          this.data = [...this.data, ...response.query.allpages.filter(r => (r.title !== '' && r.title.search('/') === -1))];
+          return true;
+        }
+        return false;
+      }).catch((err) => {
+        console.log(err);
+      });
+  }
+
+  removeNotExistLocalPages() {
+    const pagenames = [this.localData.map(o => o.title), this.data.map(o => o.title)];
+    const diffPagenames = pagenames.reduce((a, b) => a.filter(c => !b.includes(c)));
+    if (diffPagenames.length > 0) {
+      diffPagenames.forEach((pagename) => {
+        const filePath = `${this.dataPath}/${this.localData.filter(p => p.title === pagename)[0].path}`;
+        fs.unlinkSync(filePath);
+      });
+    }
   }
 
   /**
@@ -41,7 +106,7 @@ module.exports = class PageStorage {
    * @returns {String[]}
    */
   getPageList() {
-    return this.data.map(p => p.name);
+    return this.data.map(p => p.title);
   }
 
   /**
@@ -53,24 +118,20 @@ module.exports = class PageStorage {
       apiUrl: this.apiURL,
       username: this.botData.user,
       password: this.botData.password,
-    }).then(() => {
-      return this.bot.read(pagenames.join('|'), { timeout: 8000 }).then((response) => {
-        const ids = Object.keys(response.query.pages);
-        ids.forEach((id) => {
-          const pagename = response.query.pages[id]['title'];
-          const content = response.query.pages[id]['revisions'] ? response.query.pages[id]['revisions'][0]['*'] : '';
-          if (content === undefined) {
-            console.log(`There's no content for: ${pagename}`);
-          }
-          const path = this.getPathByName(pagename);
-          this.savePageToStorage({ pagename, path, content });
-        });
-      }).catch((err) => {
-        console.log(err);
+    }).then(() => this.bot.read(pagenames.join('|'), { timeout: 8000 }).then((response) => {
+      const ids = Object.keys(response.query.pages);
+      ids.forEach((id) => {
+        const pagename = response.query.pages[id].title;
+        const content = response.query.pages[id].revisions ? response.query.pages[id].revisions[0]['*'] : '';
+        if (content === undefined) {
+          console.log(`There's no content for: ${pagename}`);
+        }
+        const path = this.getPathByName(pagename);
+        this.savePageToStorage({ pagename, path, content });
       });
     }).catch((err) => {
       console.log(err);
-    });
+    }));
   }
 
   /**
@@ -93,9 +154,7 @@ module.exports = class PageStorage {
    * @returns
    */
   getPathByName(pagename) {
-    const page = this.data.find((p) => {
-      return escape(p.name) === escape(pagename);
-    });
+    const page = this.data.find(p => escape(p.name) === escape(pagename));
     return page && page.path ? page.path : `${Page.getFilenameByPagename(pagename)}`;
   }
 
@@ -103,15 +162,15 @@ module.exports = class PageStorage {
    * Get pages saved locally
    * @param {String} dir
    */
-  getLocalPages(dir) {
+  initLocalData(dir) {
     fs.readdirSync(dir).forEach((item) => {
       if (fs.lstatSync(`${dir}/${item}`, { encoding: 'utf8' }).isFile()) {
-        this.data.push({
-          name: Page.getPagenameByFilepath(`${dir}/${item}`),
+        this.localData.push({
+          title: Page.getPagenameByFilepath(`${dir}/${item}`),
           path: (`${dir}/${item}`).replace(`${this.dataPath}/`, ''),
         });
       } else if (this.ignoredPaths.indexOf(item) === -1) {
-        this.getLocalPages(`${dir}/${item}`, this.data);
+        this.initLocalData(`${dir}/${item}`);
       }
     });
   }
